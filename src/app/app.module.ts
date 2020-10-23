6ce13e9766c1b9b7d7238c823a2d442327f9d85d
Jose Luis Aguilar <jlaguigom@gmail.com>
2020-10-23 07:28:19 +0200

    Added angular material and configure basic button and input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from '../app/posts/post-create/post-create.component';
@@ -8,7 +11,7 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule, MatInputModule],
+  imports: [BrowserModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './post-create.component.html',
-  selector: 'app-post-creae'
+  selector: 'app-post-creae',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue: string;
   newPost = 'NO CONTENT';
 
-  onAddPost(postInput: HTMLTextAreaElement): void {
+  onAddPost(postInput: string): void {
     console.log(postInput);
-    this.newPost = postInput.value;
+    this.newPost = postInput;
   }
 }
