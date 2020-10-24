cda4ddd6ead8b92d85617a18fa65ef1cf9a669c9
Jose Luis Aguilar <jlaguigom@gmail.com>
2020-10-24 08:46:46 +0200

    Create header and added toolbar to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from '../app/posts/post-create/post-create.component';
+import { HeaderComponent } from '../app/header/header.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  imports: [BrowserModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
