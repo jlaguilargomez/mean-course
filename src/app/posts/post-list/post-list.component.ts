600c1316f154089d39da5120f7bc64fdb9d41d6e
Jose Luis Aguilar <jlaguigom@gmail.com>
2020-10-24 09:03:40 +0200

    Set post content array

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+interface PostContent {
+  title: string;
+  content: string;
+}
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html'
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {}
+export class PostListComponent {
+  public posts: PostContent[] = [
+    { title: 'First Post', content: 'This is the first post content' },
+    { title: 'Second Post', content: 'This is the second post content' },
+    { title: 'Third Post', content: 'This is the third post content' },
+    { title: 'Fourth Post', content: 'This is the fourth post content' }
+  ];
+}
