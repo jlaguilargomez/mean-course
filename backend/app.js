22de4f92b3750f941a87c4a7f5cd57055460ab19
Jose Luis Aguilar <jlaguigom@gmail.com>
2020-10-28 07:32:11 +0100

    Change server and add nodemon to the project

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next) => {
+  console.log('First middleware');
+  // next() permite continuar con la ejecución de middlewares
+  next()
+})
+
+app.use((req,res,next) => {
+  res.send('Hello from express')
+})
+
+module.exports = app;
